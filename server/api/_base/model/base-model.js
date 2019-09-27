02478b857c463075b0e8f7aa76b85ed883ae4e54
Pehuén <pehuen.rodriguez@nayra.coop>
2019-09-27 18:10:08 -0300

    Remove url

--- server/api/_base/model/base-model.js
+++ server/api/_base/model/base-model.js
@@ -12,7 +12,6 @@ const baseSchema = {
   deleted: { type: Boolean },
   deletedBy: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   deletedAt: { type: Date },
-  url: { type: String, trim: true, immutable: true },
 };
 
 module.exports = {
