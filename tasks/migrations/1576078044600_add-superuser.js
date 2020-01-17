7b81f04f55db8321da2fe663d25b684762ddc512
casanovajose <jose.casanova@nayra.coop>
2020-01-17 16:23:38 -0300

    #NCAC-46 account & user base migration

--- tasks/migrations/1576078044600_add-superuser.js
+++ tasks/migrations/1576078044600_add-superuser.js
@@ -5,11 +5,18 @@ const crypto = require("crypto");
 
 exports.getVersion = () => {
   return __filename.replace(path.join(__dirname, "/"), "");
-}
+};
 
-exports.up = (db, cb) => {
+exports.up = async (db, cb) => {
   const salt = crypto.randomBytes(16).toString("hex");
   const hash = crypto.pbkdf2Sync("123456", salt, 1000, 64, "sha512").toString("hex");
+
+  const account = {
+    name: "Nayra CMS",
+    privateKey: crypto.randomBytes(48).toString("hex"),
+    isSuperAdmin: true
+
+  };
 
   const user = {
     firstName: "Super",
@@ -21,13 +28,23 @@ exports.up = (db, cb) => {
     emailConfirmed: true
   };
 
-  // user or users
-  db.collection("users").insertOne(user, (err) => {
+  db.collection("accounts").insertOne(account, (err) => {
     if (err) {
       console.error(err);
       cb(err, false);
     }
 
-    cb(null, true);
+    console.log(`Created the account ${account.name}`);
+    user.accountId = account._id;
+    // user or users
+    db.collection("users").insertOne(user, (_err) => {
+      if (_err) {
+        console.error(_err);
+        cb(_err, false);
+      }
+      console.log(`Created the base user ${user.username}`);
+      console.log(`Associating ${user.username} to the account ${account.name}`);
+      cb(null, true);
+    });
   });
 };
