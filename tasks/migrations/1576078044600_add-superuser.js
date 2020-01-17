3a3d71042273a1a6b077389eea7a1dbc2d019f32
casanovajose <jose.casanova@nayra.coop>
2020-01-17 13:47:34 -0300

    fixed NCAC-44

--- tasks/migrations/1576078044600_add-superuser.js
+++ tasks/migrations/1576078044600_add-superuser.js
@@ -2,34 +2,32 @@
 // migration for ../tasks/migrations/1576078044600_add-superuser.js
 const path = require("path");
 const crypto = require("crypto");
-const mongoose = require("mongoose");
 
 exports.getVersion = () => {
   return __filename.replace(path.join(__dirname, "/"), "");
 }
 
-exports.up = async (db, cb) => {
-  const timestamp = Date.now();
-  const pass = crypto.createHash("md5").update("12341234").digest("hex");
-
-
-  const User = mongoose.model("User", UserSchema);
+exports.up = (db, cb) => {
+  const salt = crypto.randomBytes(16).toString("hex");
+  const hash = crypto.pbkdf2Sync("123456", salt, 1000, 64, "sha512").toString("hex");
 
   const user = {
-    // firstName: "Super",
-    // lastName: "Admin",
-    // username: "admin",
-    // email: "admin@nayra.coop"
-    // password: pass,
-    // createdAt: timestamp,
-    // updatedAt: timestamp
+    firstName: "Super",
+    lastName: "Admin",
+    username: "admin",
+    email: "admin@nayra.coop",
+    hash,
+    salt,
+    emailConfirmed: true
   };
 
   // user or users
-  // db.collection("users").insertOne(user, (err) => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  // });
-  cb(null, true);
+  db.collection("users").insertOne(user, (err) => {
+    if (err) {
+      console.error(err);
+      cb(err, false);
+    }
+
+    cb(null, true);
+  });
 };
