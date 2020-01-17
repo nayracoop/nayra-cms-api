2a7089698885e6106aac794cae4efd50ee5d0902
Pehuén <pehuen.rodriguez@nayra.coop>
2020-01-17 17:27:01 -0300

    delint

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 require("dotenv").config();
 require("./server/server");
